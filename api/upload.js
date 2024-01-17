a0397fb3e26ad48133eccccd3f74d231e91650d7
wrhsd1 <wangrenhuan@gmail.com>
2024-01-17 23:15:02 +0800

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,27 +1,20 @@
+const express = require('express');
 const multer = require('multer');
 const webdav = require('webdav');
+
+const app = express();
 const upload = multer({ dest: 'uploads/' });
 
-module.exports = (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(405).send('Method Not Allowed');
-    return;
-  }
-
-  upload.single('file')(req, res, (err) => {
-    if (err) {
-      res.status(500).send(err);
-      return;
-    }
-
-    const file = req.file;
-    const client = webdav.createClient(process.env.WEBDAV_URL, {
-      username: process.env.WEBDAV_USERNAME,
-      password: process.env.WEBDAV_PASSWORD,
-    });
-
-    client.putFileContents('/' + file.originalname, file.buffer).then(() => {
-      res.json({ webdavUrl: process.env.WEBDAV_URL + file.originalname });
-    });
+app.post('/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  const client = webdav.createClient(process.env.WEBDAV_URL, {
+    username: process.env.WEBDAV_USERNAME,
+    password: process.env.WEBDAV_PASSWORD,
   });
-};
+
+  client.putFileContents('/' + file.originalname, file.buffer).then(() => {
+    res.json({ webdavUrl: process.env.WEBDAV_URL + file.originalname });
+  });
+});
+
+module.exports = app;
